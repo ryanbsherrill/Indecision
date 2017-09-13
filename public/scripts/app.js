0978f2421436188456c8426a199a92d0fbf75955
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-09-13 11:50:10 -0400

    added state, all handler methods comeplete

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -19,23 +19,36 @@ var Counter = function (_React$Component) {
     _this.handlePlusOne = _this.handlePlusOne.bind(_this);
     _this.handleMinusOne = _this.handleMinusOne.bind(_this);
     _this.handleReset = _this.handleReset.bind(_this);
+    _this.state = { count: 0 };
     return _this;
   }
 
   _createClass(Counter, [{
     key: 'handlePlusOne',
     value: function handlePlusOne() {
-      console.log('handlePlusOne');
+      this.setState(function (prevState) {
+        return {
+          count: prevState.count + 1
+        };
+      });
     }
   }, {
     key: 'handleMinusOne',
     value: function handleMinusOne() {
-      console.log('handleMinusOne');
+      this.setState(function (prevState) {
+        return {
+          count: prevState.count - 1
+        };
+      });
     }
   }, {
     key: 'handleReset',
     value: function handleReset() {
-      console.log('handleReset');
+      this.setState(function () {
+        return {
+          count: 0
+        };
+      });
     }
   }, {
     key: 'render',
@@ -46,7 +59,8 @@ var Counter = function (_React$Component) {
         React.createElement(
           'h1',
           null,
-          'Count: '
+          'Count: ',
+          this.state.count
         ),
         React.createElement(
           'button',
